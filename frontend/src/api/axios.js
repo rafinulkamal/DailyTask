23238cf6f350cf244994cf30220c70e8e101009d
Shafin <rafinulkamal@gmail.com>
2026-08-14 16:41:34 +0600

    update backend URL

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://dailytask-production.up.railway.app/api",
 });
 
 // Attach token to every request if it exists
